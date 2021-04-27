f21c7c51ea26b2a74e72c4791ad5cf04e4eb891e
mariemnajai <mariem.najai@esprit.tn>
2021-04-27 17:25:38 +0100

    initialisation

---
+++ src/main/java/tn/esprit/spring/services/IEmployeService.java
@@ -0,0 +1,27 @@
+package tn.esprit.spring.services;
+
+
+import java.util.List;
+
+
+import tn.esprit.spring.entities.Employe;
+
+
+
+
+
+
+public interface IEmployeService {
+	
+	
+	public Employe getEmployeeById(int employeId);
+	public int deleteEmployeById(int employeId);
+	 public Employe updateEmploye(Employe em);
+	 public Employe addEmploye(Employe em);
+	 public List<Employe> retrieveAllEmployes();
+
+	//mariem
+	
+
+	
+}
---
+++ src/test/java/tn/esprit/spring/service/EmployeServiceImplTest.java
@@ -0,0 +1,81 @@
+
+package tn.esprit.spring.service;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+import java.util.List;
+
+
+
+
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import tn.esprit.spring.entities.Employe;
+import tn.esprit.spring.entities.Role;
+
+import tn.esprit.spring.services.IEmployeService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmployeServiceImplTest {
+	
+	
+	@Autowired 
+	IEmployeService us;
+	
+	Integer idEm;
+	@Test
+	public void testAddEmploye()  {
+		
+		Employe em = new Employe("najai", "mariem", "mariem@esprit.tn", "123456", Role.ADMINISTRATEUR); 
+		Employe employeAdded = us.addEmploye(em); 
+		assertEquals(em.getNom(),employeAdded.getNom());
+	}
+	
+	@Test
+	public void testRetrieveAllEmployes() {
+		List<Employe> listEmployes = us.retrieveAllEmployes(); 
+		 
+		assertEquals(2, listEmployes.size());
+	}
+
+	@Test
+	public void testModifyUser() throws ParseException   {
+		
+		Employe u = new Employe("najai", "mariem", "mariem@esprit.tn","12345", Role.INGENIEUR); 
+		Employe userUpdated  = us.updateEmploye(u); 
+		assertEquals(u.getPrenom(), userUpdated.getPrenom());
+	}
+	
+	@Test
+	public void testGetEmployeById() {
+		Employe e =us.getEmployeeById(8);
+		assertEquals(8, e.getId());
+	}
+	
+	@Test
+	public void testDeleteEmployeById()
+	{
+		if(idEm!=null){
+		int i = us.deleteEmployeById(idEm);
+		
+		assertEquals(0, i);}
+		else {
+			int i = us.deleteEmployeById(10);
+			
+			assertEquals(0, i);}
+	}
+	
+}
+    
+
